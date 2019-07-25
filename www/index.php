5c95399577f540683f470927e61c5f1d058d4445
GennadySX <htcucell@gmail.com>
2019-07-26 00:03:43 +0300

    fs-2507 -s 1

---
+++ www/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Docker LAMP by GennadySX</title>
+</head>
+<body>
+<style>
+    h1, p {
+        text-align: center;
+    }
+</style>
+<h1>Hi, Welcome!</h1>
+<p>I made easy lamp for you! <br> enjoy it!</p>
+<?= phpinfo();?>
+</body>
+
+</html>
